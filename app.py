355566fc4951931d01fea176f6b1156a698e093c
paladivijay30-tech <paladivijaya@gmail.com>
2026-01-15 12:40:10 +0530

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,87 @@
-# Streamlit app code will be added here
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (
+    accuracy_score, roc_auc_score, precision_score,
+    recall_score, f1_score, matthews_corrcoef
+)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+
+import xgboost as xgb
+import pickle
+
+df = pd.read_csv("C:/Users/SESA804787/OneDrive - Schneider Electric/SE/WILP/Machine Learning/Assignment 2/heart.csv")
+df.head()
+# Separate features and target
+X = df.drop("HeartDisease", axis=1)
+y = df["HeartDisease"]
+
+# Identify categorical and numeric columns
+categorical_cols = X.select_dtypes(include=["object"]).columns
+numeric_cols = X.select_dtypes(exclude=["object"]).columns
+
+# One-hot encode categoricals
+X_encoded = pd.get_dummies(X, columns=categorical_cols, drop_first=True)
+
+# Feature scaling (Standardization)
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X_encoded)
+
+# Convert back to DataFrame
+X_scaled = pd.DataFrame(X_scaled, columns=X_encoded.columns)
+
+X_scaled.head()
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42, stratify=y
+)
+
+X_train.shape, X_test.shape
+print(X_train.columns.tolist())
+
+
+
+models = {
+    "Logistic Regression": LogisticRegression(max_iter=1000),
+    "Decision Tree": DecisionTreeClassifier(),
+    "KNN": KNeighborsClassifier(),
+    "Naive Bayes": GaussianNB(),
+    "Random Forest": RandomForestClassifier(),
+    "XGBoost": xgb.XGBClassifier(eval_metric="logloss")
+}
+
+results = {}
+
+for name, model in models.items():
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    y_proba = model.predict_proba(X_test)[:, 1]
+
+    results[name] = {
+        "Accuracy": accuracy_score(y_test, y_pred),
+        "ROC_AUC": roc_auc_score(y_test, y_proba),
+        "Precision": precision_score(y_test, y_pred),
+        "Recall": recall_score(y_test, y_pred),
+        "F1 Score": f1_score(y_test, y_pred),
+        "MCC": matthews_corrcoef(y_test, y_pred),
+    }
+  results_df = pd.DataFrame(results).T
+results_df
+
+
+for name, model in models.items():
+    filename = name.replace(" ", "_") + ".pkl"
+    with open(filename, "wb") as f:
+        pickle.dump(model, f)
+
+# Save scaler too
+with open("scaler.pkl", "wb") as f:
+    pickle.dump(scaler, f)
